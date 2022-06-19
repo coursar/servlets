07cf451c349a52cfdc2b04f1a04b24ff8b0af0fa
student <student@localhost>
2022-06-19 15:47:20 +0300

    switch from xml to annotations

--- src/main/java/org/example/servlet/HelloServlet.java
+++ src/main/java/org/example/servlet/HelloServlet.java
@@ -1,9 +1,17 @@
 package org.example.servlet;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
 
 import java.io.IOException;
 
+// TODO:
+//  1. XML
+//  2. Annotations
+@WebServlet("/hi")
+
+// TODO: Servet, ServletRequest, ServletResponse
+// TODO: HttpServlet
 public class HelloServlet implements Servlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
